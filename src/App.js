91057b59a48ffc23060b21e99aec786adb07c0ff
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-01-09 17:54:24 +0100

    files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './normal.css'
+
 
 function App() {
   return (
     <div className="App">
       <aside className='sidemenu'>
-        <h1>
-          Aside
-        </h1>
+        <div className='side-menu-button'>
+          <span>
+            +
+          </span>
+          New chat
+        </div>
       </aside>
       <section className='chatbox'>
 
