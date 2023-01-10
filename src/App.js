a040756c968691b7814b9c7d35d89f48ed393b7f
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-01-10 13:13:01 +0100

    files

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
         </div>
       </aside>
       <section className='chatbox'>
-
+        <div className='chat-input-holder'>
+          <textarea rows={1} className='chat-input-textarea' placeholder='Type your message here'></textarea>
+        </div>
       </section>
     </div>
   );
